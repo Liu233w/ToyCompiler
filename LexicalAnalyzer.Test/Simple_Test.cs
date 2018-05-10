5ae01dedefd41728a8752b022466eae069d3cade
Liu233w <wwwlsmcom@outlook.com>
2018-05-10 18:44:44 +0800

    补充单元测试
    找出来一个错

--- LexicalAnalyzer.Test/Simple_Test.cs
+++ LexicalAnalyzer.Test/Simple_Test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Shouldly;
@@ -7,43 +8,154 @@ namespace LexicalAnalyzer.Test
 {
     public class Simple_Test
     {
-        [Fact]
-        public void Analyze_能够正确分析语法式()
+        [Theory]
+        [MemberData(nameof(GetCorrectCombinationsForAnalyze))]
+        public void Analyze_能够正确分析语法式(string buffer, TreeNode target)
         {
-            const string buffer = "cabd";
-
             var res = Simple.Analyze(buffer);
 
-            res.ShouldDeepEqual(new TreeNode
+            res.ShouldDeepEqual(target);
+        }
+
+        public static IEnumerable<object[]> GetCorrectCombinationsForAnalyze()
+        {
+            yield return new object[]
             {
-                Token = 'S',
-                Children = new List<Tree>
+                "cabd",
+                new TreeNode
                 {
-                    new TreeLeaf
+                    Token = 'S',
+                    Children = new List<Tree>
                     {
-                        Token = 'c',
-                    },
-                    new TreeNode
-                    {
-                        Token = 'A',
-                        Children = new List<Tree>
+                        new TreeLeaf
                         {
-                            new TreeLeaf
+                            Token = 'c',
+                        },
+                        new TreeNode
+                        {
+                            Token = 'A',
+                            Children = new List<Tree>
                             {
-                                Token = 'a',
-                            },
-                            new TreeLeaf
-                            {
-                                Token = 'b',
-                            },
-                        }
-                    },
-                    new TreeLeaf
-                    {
-                        Token = 'd',
-                    },
+                                new TreeLeaf
+                                {
+                                    Token = 'a',
+                                },
+                                new TreeLeaf
+                                {
+                                    Token = 'b',
+                                },
+                            }
+                        },
+                        new TreeLeaf
+                        {
+                            Token = 'd',
+                        },
+                    }
                 }
-            });
+            };
+
+            yield return new object[]
+            {
+                "aa",
+                new TreeNode
+                {
+                    Token = 'S',
+                    Children = new List<Tree>
+                    {
+                        new TreeLeaf
+                        {
+                            Token = 'a',
+                        },
+                        new TreeNode
+                        {
+                            Token = 'A',
+                            Children = new List<Tree>
+                            {
+                                new TreeLeaf
+                                {
+                                    Token = 'a',
+                                },
+                            }
+                        },
+                    }
+                }
+            };
+
+            yield return new object[]
+            {
+                "aab",
+                new TreeNode
+                {
+                    Token = 'S',
+                    Children = new List<Tree>
+                    {
+                        new TreeLeaf
+                        {
+                            Token = 'a',
+                        },
+                        new TreeNode
+                        {
+                            Token = 'A',
+                            Children = new List<Tree>
+                            {
+                                new TreeLeaf
+                                {
+                                    Token = 'a',
+                                },
+                                new TreeLeaf
+                                {
+                                    Token = 'b',
+                                },
+                            }
+                        },
+                    }
+                }
+            };
+
+            yield return new object[]
+            {
+                "cad",
+                new TreeNode
+                {
+                    Token = 'S',
+                    Children = new List<Tree>
+                    {
+                        new TreeLeaf
+                        {
+                            Token = 'c',
+                        },
+                        new TreeNode
+                        {
+                            Token = 'A',
+                            Children = new List<Tree>
+                            {
+                                new TreeLeaf
+                                {
+                                    Token = 'a',
+                                },
+                            }
+                        },
+                        new TreeLeaf
+                        {
+                            Token = 'd',
+                        },
+                    }
+                }
+            };
+
+        }
+
+        [Theory]
+        [InlineData("zzz")]
+        [InlineData("ab")]
+        [InlineData("abd")]
+        [InlineData("cd")]
+        [InlineData("")]
+        [InlineData("aa ")]
+        public void Analyze_在输入不正确的句子时能够检测到错误(string buffer)
+        {
+            var res = Simple.Analyze(buffer);
+            res.ShouldBeNull();
         }
     }
 
