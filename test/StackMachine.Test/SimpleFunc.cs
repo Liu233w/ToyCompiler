9807e8500cc6589a5fa2edc31182de2743760683
Liu233w <wwwlsmcom@outlook.com>
2018-05-16 12:24:28 +0800

    添加简单的单元测试

---
+++ test/StackMachine.Test/SimpleFunc.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Liu233w.StackMachine.Instructions;
+
+namespace Liu233w.StackMachine.Test
+{
+    public class SimpleFunc : StackFrameFunction
+    {
+        private int _param;
+
+        private List<int> _simpleList;
+
+        private int _variable;
+
+        public SimpleFunc(int param, List<int> simpleList)
+        {
+            _param = param;
+            _simpleList = simpleList;
+        }
+
+        public SimpleFunc(SimpleFunc that)
+            : base(that)
+        {
+            _variable = that._variable;
+            _param = that._param;
+            // 不对 list 进行深拷贝
+            _simpleList = that._simpleList;
+        }
+
+        public override object Clone()
+        {
+            return new SimpleFunc(this);
+        }
+
+        protected override FuncInstructionBase StepMove(int pc, object lastReturned)
+        {
+            switch (pc)
+            {
+                case 0:
+                {
+                    _simpleList.Add(_param);
+                    _variable = _param;
+                    return Continue();
+                }
+                case 1:
+                {
+                    ++_variable;
+                    if (_variable < 10)
+                    {
+                        _simpleList.Add(_variable);
+                        return GoTo(1);
+                    }
+                    else
+                    {
+                        return Return((_param, _variable));
+                    }
+                }
+                default:
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ test/StackMachine.Test/StackMachineTest.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Liu233w.StackMachine.Test
+{
+    public class StackMachineTest
+    {
+        private StackMachine _machine;
+
+        public StackMachineTest()
+        {
+            _machine = new StackMachine();
+        }
+
+        [Fact]
+        public void 简单的函数()
+        {
+            var list = new List<int>();
+            var result = _machine.Run(new SimpleFunc(0, list));
+            var (r1, r2) = ((int, int))result;
+
+            list.Should().BeEquivalentTo(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            r1.Should().Be(0);
+            r2.Should().Be(10);
+        }
+    }
+}
