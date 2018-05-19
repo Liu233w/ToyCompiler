5d38df3dc721488e49add96f4ceb2125c64bf04d
Liu233w <wwwlsmcom@outlook.com>
2018-05-19 15:22:46 +0800

    使用 release 模式运行实验代码

--- 实验课作业/EX1/EX1.cmd
+++ 实验课作业/EX1/EX1.cmd
@@ -10,7 +10,7 @@ echo 编辑此脚本以修改需要分析的源代码文件
 
 pause
 
-dotnet run -p ..\..\src\Experiments\EX1 -- .\词法分析程序实验要求\测试输入源文件\test1.txt >tokenOut.txt 2>&1
+dotnet run -c release -p ..\..\src\Experiments\EX1 -- .\词法分析程序实验要求\测试输入源文件\test1.txt >tokenOut.txt 2>&1
 
 echo 运行结束
 pause
