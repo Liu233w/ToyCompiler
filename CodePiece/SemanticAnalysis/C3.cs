979c080435a26371c7563ad67a5b6df5a66e2972
Liu233w <wwwlsmcom@outlook.com>
2018-06-05 11:55:16 +0800

    t

---
+++ CodePiece/SemanticAnalysis/C3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Liu233w.Compiler.CodePiece.SemanticAnalysis
+{
+    public class C3
+    {
+        public static Dictionary<string, string> DTypeTable = new Dictionary<string, string>();
+
+        public static string DType;
+        
+        public static void CalDType(TreeNode tree)
+        {
+            switch (tree)
+            {
+                case Type t:
+                {
+                    DType = t.OType.ToString();
+                    break;
+                }
+                case Decl d:
+                {
+                    CalDType(d.Type);
+                    CalDType(d.VarList);
+                    break;
+                }
+                case VarList l:
+                {
+                    DTypeTable[l.Id.Lexem] = DType;
+                    if (l.VarList2 != null)
+                    {
+                        CalDType(l.VarList2);
+                    }
+                    break;
+                }
+                default:
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public class TreeNode
+        {
+        }
+
+        public class Decl : TreeNode
+        {
+            public Type Type { get; set; }
+
+            public VarList VarList { get; set; }
+        }
+
+        public class Type : TreeNode
+        {
+            public OType OType { get; set; }
+        }
+
+        public enum OType
+        {
+            Int,
+            Float,
+        }
+
+        public class VarList : TreeNode
+        {
+            public Id Id { get; set; }
+
+            public VarList VarList2 { get; set; }
+        }
+
+        public class Id : TreeNode
+        {
+            public string Lexem { get; set; }
+        }
+    }
+}
---
+++ CodePiece/SemanticAnalysis/C3Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Liu233w.Compiler.CodePiece.SemanticAnalysis
+{
+    public class C3Test
+    {
+        [Fact]
+        public void TestCalculate()
+        {
+            var tree = new C3.Decl
+            {
+                Type = new C3.Type
+                {
+                    OType = C3.OType.Int,
+                },
+                VarList = new C3.VarList
+                {
+                    Id = new C3.Id
+                    {
+                        Lexem = "a",
+                    },
+                    VarList2 = new C3.VarList
+                    {
+                        Id = new C3.Id
+                        {
+                            Lexem = "b",
+                        },
+                        VarList2 = null,
+                    }
+                }
+            };
+
+            C3.CalDType(tree);
+
+            C3.DType.Should().Be("Int");
+            C3.DTypeTable.Should().Contain(
+                new KeyValuePair<string, string>("a", "Int"),
+                new KeyValuePair<string, string>("b", "Int"));
+        }
+    }
+}
