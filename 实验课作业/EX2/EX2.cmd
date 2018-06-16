435ba9183dc9ea1c351b694a666a6d32599666be
Liu233w <wwwlsmcom@outlook.com>
2018-06-16 23:34:36 +0800

    添加EX2运行程序

---
+++ 实验课作业/EX2/EX2.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+CHCP 65001
+
+echo 将会把运行结果写入到本目录下的 astOut.txt 文件中
+
+echo 运行本程序需要 .net core sdk 2.0 以上
+
+echo 编辑此脚本以修改需要分析的源代码文件
+
+pause
+
+dotnet run -c release -p ..\..\src\Experiments\EX2 -- .\语法分析课程实验要求\测试输入源文件\test1.txt >astOut.txt 2>&1
+
+echo 运行结束
+pause
