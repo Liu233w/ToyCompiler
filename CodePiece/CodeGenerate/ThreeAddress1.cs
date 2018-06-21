77a4f5904a26bff4f0727a7c284e422a6641cda1
Liu233w <wwwlsmcom@outlook.com>
2018-06-21 11:53:23 +0800

    add three address 1

---
+++ CodePiece/CodeGenerate/ThreeAddress1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Liu233w.Compiler.CodePiece.CodeGenerate
+{
+    public class ThreeAddress1
+    {
+        private StringBuilder _code;
+
+        private TreeNode _tree;
+
+        private int _tempId;
+
+        public ThreeAddress1 WithTree(TreeNode newTree)
+        {
+            _tree = newTree;
+            return this;
+        }
+
+        public string Build()
+        {
+            _code = new StringBuilder();
+            _tempId = 0;
+            DoGenerate(_tree);
+            return _code.ToString();
+        }
+
+        private void DoGenerate(TreeNode node)
+        {
+            if (node.Kind == NodeKind.OpKind)
+            {
+                DoGenerate(node.LChild);
+                DoGenerate(node.RChild);
+                node.StrVal = GetNewTempId();
+                string op;
+
+                switch (node.Op)
+                {
+                    case Optype.Plus:
+                        op = "+";
+                        break;
+                    case Optype.Minus:
+                        op = "-";
+                        break;
+                    case Optype.Multiply:
+                        op = "*";
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Emit($"{node.StrVal} = {node.LChild.StrVal} {op} {node.RChild.StrVal}");
+            }
+
+            // 不管其他情况
+        }
+
+        private void Emit(string codeLine)
+        {
+            _code.AppendLine(codeLine);
+        }
+
+        private string GetNewTempId()
+        {
+            return $"t{++_tempId}";
+        }
+
+        public enum Optype
+        {
+            Plus,
+            Minus,
+            Multiply,
+        }
+
+        public enum NodeKind
+        {
+            OpKind,
+            ConstKind,
+            IdKind,
+        }
+
+        public class TreeNode
+        {
+            public NodeKind Kind { get; set; }
+
+            public Optype Op { get; set; }
+
+            public TreeNode LChild { get; set; }
+
+            public TreeNode RChild { get; set; }
+
+            public string StrVal { get; set; }
+        }
+    }
+}
---
+++ CodePiece/CodeGenerate/ThreeAddress1Test.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Liu233w.Compiler.CodePiece.CodeGenerate
+{
+    public class ThreeAddress1Test
+    {
+        [Fact]
+        public void TestGenerate()
+        {
+            // (1+a)*(b-2)
+
+            var tree = new ThreeAddress1.TreeNode
+            {
+                Kind = ThreeAddress1.NodeKind.OpKind,
+                Op = ThreeAddress1.Optype.Multiply,
+                LChild = new ThreeAddress1.TreeNode
+                {
+                    Kind = ThreeAddress1.NodeKind.OpKind,
+                    Op = ThreeAddress1.Optype.Plus,
+                    LChild = new ThreeAddress1.TreeNode
+                    {
+                        Kind = ThreeAddress1.NodeKind.ConstKind,
+                        StrVal = "1",
+                    },
+                    RChild = new ThreeAddress1.TreeNode
+                    {
+                        Kind = ThreeAddress1.NodeKind.IdKind,
+                        StrVal = "a",
+                    },
+                },
+                RChild = new ThreeAddress1.TreeNode
+                {
+                    Kind = ThreeAddress1.NodeKind.OpKind,
+                    Op = ThreeAddress1.Optype.Minus,
+                    LChild = new ThreeAddress1.TreeNode
+                    {
+                        Kind = ThreeAddress1.NodeKind.IdKind,
+                        StrVal = "b",
+                    },
+                    RChild = new ThreeAddress1.TreeNode
+                    {
+                        Kind = ThreeAddress1.NodeKind.ConstKind,
+                        StrVal = "2",
+                    }
+                }
+            };
+
+            var code = new ThreeAddress1()
+                .WithTree(tree)
+                .Build();
+
+            code.Should().Be(@"t1 = 1 + a
+t2 = b - 2
+t3 = t1 * t2
+");
+        }
+    }
+}
